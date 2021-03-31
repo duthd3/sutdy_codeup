2432325baaf0a973194c4d1bfbeeb4b6a0d2956a
user name <duthd3@naver.com>
2021-03-31 22:29:05 +0900

    Create 23.c

---
+++ 21_03_31/23.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(void)
+{
+	int num1,num2;
+	char c;
+
+	scanf("%d%c%d", &num1,&c,&num2);
+	printf("%d \n", num1);
+	printf("%d", num2);
+	return 0;
+}
