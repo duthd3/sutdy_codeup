7f011f0e01b9609fd882c5cf1f29b50f31d65935
YeoSong <duthd3@naver.com>
2021-04-11 23:54:12 +0900

    Create 64.c

---
+++ 21_04_11/64.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int num1, num2, num3;
+	scanf_s("%d %d %d", &num1, &num2, &num3);
+	printf("%d \n", (num1 > num2 ? num2 : num1) > num3 ? num3 : (num1 > num2 ? num2 : num1));
+	return 0;
+
+}
